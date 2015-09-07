go-swagger/go-swagger

Move packages to go-swagger org

client/response.go
@@ -3,7 +3,7 @@ package client
 import (
 	"io"
 
-	"github.com/casualjim/go-swagger/httpkit"
+	"github.com/go-swagger/go-swagger/httpkit"
 )
 
 // A Response represents a client response

client/response_test.go
@@ -6,7 +6,7 @@ import (
 	"io/ioutil"
 	"testing"
 
-	"github.com/casualjim/go-swagger/httpkit"
+	"github.com/go-swagger/go-swagger/httpkit"
 	"github.com/stretchr/testify/assert"
 )
 

go-openapi/runtime

client/response.go
@@ -4,7 +4,7 @@ import (
 	"io"
 	"net/http"
 
-	"github.com/casualjim/go-swagger/client"
+	"github.com/go-swagger/go-swagger/client"
 )
 
 var _ client.Response = response{}

client/response_test.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 	"testing"
 
-	"github.com/casualjim/go-swagger/client"
+	"github.com/go-swagger/go-swagger/client"
 	"github.com/stretchr/testify/assert"
 )
 