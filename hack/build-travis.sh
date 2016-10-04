369afcb5cc262bead5db4af84e83334f4a7a9be6
Ivan Porto Carrero <ivan@flanders.co.nz>
2016-10-03 21:03:14 -0700

    update build script

---
+++ hack/build-travis.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e -o pipefail
+
+mkdir -p /drone/{testresults,coverage,dist}
+go test -race -timeout 20m -v ./... | go-junit-report -dir /drone/testresults
+
+# Run test coverage on each subdirectories and merge the coverage profile.
+echo "mode: ${GOCOVMODE-atomic}" > coverage.txt
+
+# Standard go tooling behavior is to ignore dirs with leading underscores
+# skip generator for race detection and coverage
+for dir in $(go list ./...)
+do
+  pth="$GOPATH/src/$dir"
+  go test -covermode=${GOCOVMODE-atomic} -coverprofile=${pth}/profile.out $dir
+  if [ -f $pth/profile.out ]
+  then
+      cat $pth/profile.out | tail -n +2 >> coverage.txt
+      # rm $pth/profile.out
+  fi
+done
+
+go tool cover -func coverage.txt
