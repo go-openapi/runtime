9cb8769cec287c4dd46338718f3aa1058b8c9531
Danny Cheung <7041787+danny-cheung@users.noreply.github.com>
2019-11-26 14:22:35 +1100

    Dereference pointer when determining destination data type

--- bytestream.go
+++ bytestream.go
@@ -77,7 +77,7 @@ func ByteStreamConsumer(opts ...byteStreamOpt) Consumer {
 		}
 
 		if t := reflect.TypeOf(data); data != nil && t.Kind() == reflect.Ptr {
-			v := reflect.Indirect(reflect.ValueOf(data))
+			v := *data
 			if t = v.Type(); t.Kind() == reflect.Slice && t.Elem().Kind() == reflect.Uint8 {
 				v.SetBytes(b)
 				return nil
