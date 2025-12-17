58c99b2e8ffd8173c9a940bb8deb772b441814d7
Frédéric BIDON <fredbi@yahoo.com>
2025-12-17 10:02:01 +0100

    chore(licensing): added correct code attribution for the denco middleware
    Signed-off-by: Frédéric BIDON <fredbi@yahoo.com>

--- middleware/denco/router.go
+++ middleware/denco/router.go
@@ -1,5 +1,7 @@
 // SPDX-FileCopyrightText: Copyright 2015-2025 go-swagger maintainers
 // SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright (c) 2014 Naoya Inada <naoina@kuune.org>
+// SPDX-License-Identifier: MIT
 
 // Package denco provides fast URL router.
 package denco
