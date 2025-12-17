bbf14dd9a17a60da8abda44bf19ef229373249f6
Frédéric BIDON <fredbi@yahoo.com>
2025-12-17 10:16:21 +0100

    doc: added doc.go headers for godoc
    Signed-off-by: Frédéric BIDON <fredbi@yahoo.com>

---
+++ client-middleware/opentracing/doc.go
@@ -0,0 +1,7 @@
+// SPDX-FileCopyrightText: Copyright 2015-2025 go-swagger maintainers
+// SPDX-License-Identifier: Apache-2.0
+
+// Package opentracing supports opentracing instrumentation for the runtime client.
+//
+// It is provided for backward-compatibility for users who can't transition to OTEL.
+package opentracing
