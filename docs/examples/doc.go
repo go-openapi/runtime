1fd10af05ff76b283c381f8a3e5b41359d17c4cb
Frederic BIDON <fredbi@yahoo.com>
2026-05-17 13:20:26 +0200

    doc: fixup module layout
    Signed-off-by: Frederic BIDON <fredbi@yahoo.com>

---
+++ docs/examples/doc.go
@@ -0,0 +1,8 @@
+// Package examples contains many code examples.
+//
+// They illustrate how to use [github.com/go-openapi/runtime].
+//
+// Code snippets are injected in the [documentation site].
+//
+// [documentation site]: https://go-openapi.github.io/runtime>
+package examples
